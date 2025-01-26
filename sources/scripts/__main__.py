9d98055985a5f2127ea79fd1919830994eeae121
Abd El Ouadoud KHOURI <128540597+AbdElOuadoudK@users.noreply.github.com>
2025-01-26 12:49:11 +0100

    Update __main__.py

--- sources/scripts/__main__.py
+++ sources/scripts/__main__.py
@@ -1,7 +1,24 @@
-current = ...
+from imagproc_pkg import *
+from pandas import read_csv, DataFrame, concat
+from os import listdir
+from os.path import join
+
+
+parser = ResumeParser()
+
+resumes_df = DataFrame(columns=['Contents', 'Boundings', 'ParseFlag'])
+# IF current is above... 
+#resumes_df = read_csv("AllResumes_.csv", index_col=["Unnamed: 0"])
+
+files = [f for f in listdir(RESUMES_PATH) if f.endswith('.png')]
+files.sort(key=lambda x: (int(x.split('_')[1]), int(x.split('_')[3].split('.')[0])))
+
+
+#------------------ SECTION TO BE ITERATED
+current = 1
 
 for i, file_name in enumerate(files, start=1):
-    if i <= current or  (not (i <= ...)) :
+    if i <= current or  (not (i <= 89)) :
         continue
     
     if i % 10 == 0 :
@@ -10,7 +27,7 @@ for i, file_name in enumerate(files, start=1):
     img = cv2.imread(resume(file_name))
     
     try:
-        lines, flag_ = CroppeImage(img, kernel_max_size=19, padding=1)
+        lines, flag_ = parser.CroppeImage(img)
         
         text_list = []
         if flag_ == 'PSM11' :
@@ -33,3 +50,6 @@ for i, file_name in enumerate(files, start=1):
     resumes_df = concat([resumes_df, temp_resume_df])
     
     print(f"Image {i} processed : [{flag_}] ")
+
+
+resumes_df.to_csv(join(DATA_PATH,"AllResumes_.csv"))
