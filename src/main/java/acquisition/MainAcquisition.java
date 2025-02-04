2f717f15567024414555fbd6a463db737946b07f
Abd El Ouadoud KHOURI <128540597+AbdElOuadoudK@users.noreply.github.com>
2025-02-04 12:34:10 +0100

    Update MainAcquisition.java
    minor change (basepath's folder changed into `.../out/aquisition`)

--- src/main/java/acquisition/MainAcquisition.java
+++ src/main/java/acquisition/MainAcquisition.java
@@ -93,16 +93,14 @@ public class MainAcquisition {
 
     public static void main(String[] args) {
         MainAcquisition mainAcquisition = new MainAcquisition();
+        String DOCS_DIR = "src/main/resources/resumes/";
+        String basePath = "src/main/resources/out/acq-";
+        Boolean verbose = true;
+        Boolean clutter = false;
+        long file_max_size = 10 * 1024 * 1024;
+        Boolean async = false;
 
         try {
-            String DOCS_DIR = "src/main/resources/resumes/";
-            String basePath = "src/main/resources/out/acquisition-";
-            Boolean verbose = true;
-            Boolean clutter = false;
-            long file_max_size = 10 * 1024 * 1024;
-            Boolean async = false;
-
-
             String fileName = "resume-x.pdf";
             CompletableFuture<List<BufferedImage>> futureImages = mainAcquisition.processDocument(DOCS_DIR + fileName, verbose, file_max_size, clutter, async);
             futureImages.thenAccept(processedImages -> {
